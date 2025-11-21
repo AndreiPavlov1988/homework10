8b69b2f48b44c1f0542b54c34bd15b76a9254491
AndreiPavlov1988 <ai_pavlov@mail.ru>
2025-11-21 14:24:50 +0300

    task 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
         public static void main(String[] args) {
             checkLeapYear(2024);
+            suggestApp(0, 2014);
         }
 
         public static void checkLeapYear(int year) {
@@ -10,4 +11,13 @@ public class Main {
                 System.out.println(year + " год — невисокосный год");
             }
         }
+      //task 2
+      public static void suggestApp(int osType, int deviceYear) {
+          int currentYear = 2024;
+          String osName = osType == 0 ? "iOS" : "Android";
+          String version = deviceYear < currentYear ? "облегченную" : "";
+
+          System.out.println("Установите " + version + " версию приложения для " + osName + " по ссылке");
+      }
+
     }
